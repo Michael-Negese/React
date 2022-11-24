a09d710d9e82a195e5fbb57a02de559de1653ea4
Michael-Negese <yabidesta@gmail.com>
2022-11-24 15:14:02 -0500

    Second Commit

---
+++ Hooks/EffectHook/hookeffect/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import HookCounterONe from './Components/HookCounterONe';
+import HookMouse from './Components/HookMouse';
+import MouseContanier from './Components/MouseContanier';
+import IntervalHookCounter from './Components/IntervalHookCounter';
+import DataFetching from './Components/DataFetching';
+function App() {
+  return (
+    <div className="App">
+     {/* <HookCounterONe/> */}
+     {/* <HookMouse/> */}
+     {/* <MouseContanier/> */}
+     {/* <IntervalHookCounter/> */}
+     <DataFetching/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Hooks/EffectHook/hookeffect/src/Components/DataFetching.js
@@ -0,0 +1,33 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
+
+function DataFetching() {
+    const [post,setPost] = useState({})
+    const [id,setId] = useState(1)
+    const [idFromButtonClick,setIdFromButtonClick]  = useState(1)
+    useEffect(()=>{
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
+        .then(res=>{
+            console.log(res)
+            setPost(res.data)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    },[idFromButtonClick]);
+    const handleClick = () => {
+        setIdFromButtonClick(id);
+    };
+  return (
+    <div>
+        <input type="text" value={id} onChange={e=>setId(e.target.value)}/>
+        <button onClick={handleClick}>Click to change the title</button>
+        <div>{post.title}</div>
+        {/* <ul>
+           { post.map(post => <li key={post.id}>{post.title}</li>)}
+        </ul> */}
+    </div>
+  )
+}
+
+export default DataFetching
